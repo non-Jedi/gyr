a87d36618bb9b4649c2c91e3923ecdf159f26a71
Adam Beckmeyer <adam_git@thebeckmeyers.xyz>
2017-02-22 16:52:23 +0100

    Use uuid module to generate txn ids

--- matrix_relay/utils.py
+++ matrix_relay/utils.py
@@ -17,6 +17,7 @@
 # <http://www.gnu.org/licenses/>.
 
 import string
+import uuid
 
 
 def create_relayer(user_id):
@@ -34,8 +35,9 @@ def get_rooms():
              "to": ["!UmaZBKYIFrIPVGoUYp:localhost"]}}
 
 
-def get_txn_id():
-    return 1
+def new_txn_id():
+    """Returns a unique txn id."""
+    return str(uuid.uuid1())
 
 
 def mxid2relayuser(mxid):
@@ -57,6 +59,6 @@ def relay_message(content, sender, rooms, api):
     for room in rooms:
         # This is all well and good, but how do I choose the sender?
         # Usually the HS would identify sender by token.
-        api.send_event(room, "m.room.message", get_txn_id(),
+        api.send_event(room, "m.room.message", new_txn_id(),
                        content=content,
                        params={"user_id": relayer})
