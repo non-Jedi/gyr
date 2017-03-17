6f9beedaa399e93ae61a33d93f0caa08dbbc42d5
Adam Beckmeyer <adam_git@thebeckmeyers.xyz>
2017-03-17 21:13:44 +0100

    Removed most of the cruft from Matrix Relay.
    Still need to sort through the utils and write a good README

---
+++ gyr/resources.py
@@ -0,0 +1,53 @@
+# Copyright 2016 Adam Beckmeyer
+#
+# This file is part of Gyr.
+#
+# Gyr is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Gyr is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gyr.  If not, see <http://www.gnu.org/licenses/>.
+
+import falcon
+
+
+class Resource:
+    """Master class for falcon http resources."""
+    pass
+
+
+class Room(Resource):
+    """Generates objects to respond to GET /rooms/{room_alias}."""
+
+    def on_get(self, request, response, name=None):
+        """Called when a GET request is sent to /rooms/{room_alias}"""
+        response.status = falcon.HTTP_404
+        response.body = "{}"
+        # todo
+
+
+class Transaction(Resource):
+    """Generates objects to respond to PUT /transactions/{txn_id}"""
+
+    def on_put(self, request, response, txn_id=None):
+        """Responds to PUT request containing events."""
+        response.status = falcon.HTTP_404
+        response.body = "{}"
+        # todo
+
+
+class User(Resource):
+    """Generates objects to respond to GET /users/{user_id}"""
+
+    def on_get(self, request, response, user_id=None):
+        """Responds to GET request for users."""
+        response.status = falcon.HTTP_404
+        response.body = "{}"
+        # todo
---
+++ gyr/server.py
@@ -0,0 +1,34 @@
+# Copyright 2016 Adam Beckmeyer
+#
+# This file is part of Gyr.
+#
+# Gyr is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Gyr is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Gyr.  If not, see <http://www.gnu.org/licenses/>.
+
+import falcon
+from . import resources
+
+
+class Application(falcon.API):
+
+    def __init__(self, *args, **kwargs):
+        # This runs the __init__ method of falcon.API
+        super().__init__(*args, **kwargs)
+
+        # Add all the normal matrix API routes
+        room = resources.Room()
+        transaction = resources.Transaction()
+        user = resources.User()
+        self.add_route("/rooms/{room_alias}", room)
+        self.add_route("/transactions/{txn_id}", transaction)
+        self.add_route("/users/{user_id}", user)
