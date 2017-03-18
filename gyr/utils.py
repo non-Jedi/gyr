789da41cc60eea89b9668a117560e381f5d03ff8
Adam Beckmeyer <adam_git@thebeckmeyers.xyz>
2017-03-18 21:07:43 +0100

    Add utilities for working with mxids.

--- gyr/utils.py
+++ gyr/utils.py
@@ -16,6 +16,7 @@
 # along with Gyr.  If not, see
 # <http://www.gnu.org/licenses/>.
 
+from string import ascii_lowercase, digits
 import uuid
 from . import iofs
 
@@ -35,6 +36,22 @@ def get_users(base_path):
 def gen_txn_id():
     """Returns a new, unique txn id."""
     return str(uuid.uuid1())
+
+
+def is_full_mxid(user_string):
+    """Returns True if a string is a valid mxid."""
+    if not user_string[0] == "@":
+        return False
+    parts = user_string[1:].split(":")
+    localpart_chars = ascii_lowercase + digits + "._-="
+    if not (len(parts) == 2 and all([i in localpart_chars for i in parts[0]])):
+        return False
+    return True
+
+
+def mxid2localpart(mxid):
+    """Returns the localpart of a valid mxid."""
+    return mxid.strip("@").split(":")[0]
 
 
 def new_data(data, full_storage_path):
