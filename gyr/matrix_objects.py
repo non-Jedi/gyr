9f4351ca4f4b63d3963442fc7494ff5c1097699f
Adam Beckmeyer <adam_git@thebeckmeyers.xyz>
2017-03-20 17:47:03 +0100

    Remove more of the matrix_relay cruft
    I think this should be the last of the matrix_relay cruft, but we'll see
    I suppose.

--- gyr/matrix_objects.py
+++ gyr/matrix_objects.py
@@ -23,44 +23,21 @@ from . import iofs
 class MatrixUser:
     """Represents matrix user account."""
 
-    def __init__(self, mxid, storage_path, api):
+    def __init__(self, mxid, storage_path=None, api=None, data=None):
         self.mxid = mxid
         self.localpart, self.domain = mxid.strip("@").split(":")
+        # Storage_path is only applicable if using iofs backend
         self.storage_path = storage_path
         self.api = api
-
-    def create_relayer(self, storage_path=None, api=None):
-        """Creates MatrixUser representing "relayer" of current user."""
-        # This method exists because why wouldn't we want recursive classes?
-        if not storage_path:
-            storage_path = self.storage_path
-        if not api:
-            api = self.api
-
-        relayer_mxid = "@" + utils.mxid2relayuser(self.mxid) + ":_._"
-        self.relayer = MatrixUser(relayer_mxid, storage_path, api)
-        # The database is canonical source for whether registered
-        if self.mxid not in utils.get_users(self.storage_path):
-            self.api.as_register(self.relayer.localpart)
-            user_path = iofs.resolve_path(self.mxid, self.storage_path)
-            iofs.save_data(user_path, self.relayer.mxid)
-        else:
-            self.load(self.mxid)
+        self.data = data
 
     def load(self, mxid):
-        """Changes object to reflect mxid from db."""
+        """loads data from iofs database."""
         self.mxid = mxid
         self.localpart, self.domain = mxid.strip("@").split(":")
 
         mxid_path = iofs.resolve_path(mxid, self.storage_path)
-        mxid_data = iofs.retrieve_data(mxid_path)
-        # Data should be simple string with relayer mxid
-        self.relayer = MatrixUser(mxid_data, self.storage_path, self.api)
-
-    def relay_message(self, content, rooms):
-        """Uses self.relayer to send message to HS."""
-        self.relayer.send_message(content, rooms)
-        # Does the message need to be saved to db? Don't think so...
+        self.data = iofs.retrieve_data(mxid_path)
 
     def send_message(self, content, rooms):
         """Sends m.room.message using self.api."""
