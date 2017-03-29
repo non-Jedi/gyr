d50ab1e4b6dbb11638224d953ba2e1aa8ff733e4
Adam Beckmeyer <adam_git@thebeckmeyers.xyz>
2017-03-29 10:05:58 -0400

    Implement checking for whether txn_id is new

--- gyr/resources.py
+++ gyr/resources.py
@@ -20,9 +20,20 @@ import falcon
 
 class Resource:
     """Master class for falcon http resources."""
+
     def __init__(self, handler, hs_api):
         self.handler = handler
         self.hs_api = hs_api
+        self.tracker = [None] * 20
+
+    def _is_new(self, identifier):
+        """Returns True if identifier hasn't been seen before."""
+        if identifier in self.tracker:
+            return False
+        else:
+            self.tracker.append(identifier)
+            self.tracker.pop(0)
+            return True
 
 
 class Room(Resource):
@@ -30,7 +41,6 @@ class Room(Resource):
 
     def on_get(self, request, response, name=None):
         """Called when a GET request is sent to /rooms/{room_alias}"""
-        response.status = falcon.HTTP_404
         response.body = "{}"
         # todo: pass details of request body to self.handler
         # todo: register new room if self.handler returns True
@@ -41,9 +51,20 @@ class Transaction(Resource):
 
     def on_put(self, request, response, txn_id=None):
         """Responds to PUT request containing events."""
-        response.status = falcon.HTTP_200
         response.body = "{}"
-        # todo: verify txn_id is new
+
+        # Check whether repeat txn_id
+        if not self._is_new(txn_id):
+            response.status = falcon.HTTP_200
+            return
+
+        request.context["body"] = request.stream.read()
+        try:
+            events = json.loads(request.context["body"].decode("utf-8"))["events"]
+        except(KeyError, ValueError, UnicodeDecodeError):
+            response.status = falcon.HTTP_400
+            response.body = "Malformed request body"
+            return
         # todo: pass details of request body to self.handler
 
 
@@ -52,7 +73,6 @@ class User(Resource):
 
     def on_get(self, request, response, user_id=None):
         """Responds to GET request for users."""
-        response.status = falcon.HTTP_404
         response.body = "{}"
         # todo: pass details of request body to self.handler
         # todo: register new user if self.handler returns True
