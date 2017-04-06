a5c5bf5d99edf08090ba8453868e8df86cdda1cb
Adam Beckmeyer <adam_git@thebeckmeyers.xyz>
2017-04-05 22:04:29 -0400

    Begin adding methods to MatrixUser class

--- gyr/matrix_objects.py
+++ gyr/matrix_objects.py
@@ -16,7 +16,7 @@
 # along with Gyr.  If not, see
 # <http://www.gnu.org/licenses/>.
 
-from .api import MatrixASHttpAPI
+from . import utils
 
 
 class Event:
@@ -42,7 +42,8 @@ class Event:
         try:
             return self._user
         except AttributeError:
-            self._user = MatrixUser(self.json["user_id"], self.api_factory)
+            mxid = self.json["user_id"]
+            self._user = MatrixUser(mxid, self.api_factory(mxid))
             return self._user
 
     @property
@@ -51,8 +52,29 @@ class Event:
         try:
             return self._room
         except AttributeError:
-            self._room = MatrixRoom(self.json["room_id"], self.api_factory)
+            room_id = self.json["room_id"]
+            self._room = MatrixRoom(room_id, self.api_factory())
             return self._room
+
+    @property
+    def type(self):
+        """Returns the type of event."""
+        return self.json["type"]
+
+    @property
+    def content(self):
+        """Returns content of event."""
+        return self.json["content"]
+
+    @property
+    def timestamp(self):
+        """Returns the timestamp in milliseconds."""
+        return self.json["origin_server_ts"]
+
+    @property
+    def id(self):
+        """Returns the event id."""
+        return self.json["room_id"]
 
 
 class EventStream:
@@ -68,8 +90,9 @@ class EventStream:
 
         Args:
             json(list): List from deserializing txn from homeserver.
+            api_factory(func): Generates http api when passed mxid.
         """
-        self._json = json
+        self.json = json
         self._index = 0
         self.api_factory = api_factory
 
@@ -82,7 +105,7 @@ class EventStream:
             raise StopIteration
         else:
             self._index += 1
-            return Event(self._json[self._index], self.api_factory)
+            return Event(self.json[self._index], self.api_factory)
 
 
 class MatrixRoom:
@@ -93,38 +116,111 @@ class MatrixRoom:
 class MatrixUser:
     """Represents matrix user account."""
 
-    def __init__(self, mxid, api_factory):
+    def __init__(self, mxid, api=None):
         self.mxid = mxid
-        if api:
-            self.api = api
-        else:
-            self.api = MatrixASHttpAPI()
+        self.api = api
 
-    def send_message(self, content, rooms):
-        """Sends m.room.message using self.api."""
-        for room in rooms:
-            pass
+    def register(self):
+        """Registers self.mxid with homeserver."""
+        return self.api.register(utils.mxid2localpart(self.mxid))
 
-    def fetch_disp_name(self):
-        """Uses self.api to fetch display name and set as self.disp_name."""
-        pass
+    def create_room(self, alias=None, is_public=False, invitees=()):
+        """Calls /createRoom as self.mxid."""
+        return self.api.create_room(alias=alias, is_public=is_public,
+                                    invitees=invitees)
 
-    def set_disp_name(self, new_name):
-        """Uses self.api to set display name."""
-        self.display_name = new_name
+    def join(self, room):
+        """Joins room id or alias even if it must first be created."""
+        raise NotImplementedError("Bug Adam to get on this. He really shouldn't have released like this.")
 
-    def join_room(self, room):
-        """Uses self.api to join a matrix room."""
-        pass
+    def send_location(self, *args, **kwargs):
+        """Sends m.location message event."""
+        return self.api.send_location(*args, **kwargs)
 
-    def leave_room(self, room):
-        """Uses self.api to leave a matrix room."""
-        pass
+    def send_message(self, *args, **kwargs):
+        """Sends m.room.message with default msgtype m.text."""
+        return self.api.send_message(*args, **kwargs)
 
-    def invite(self, mxid, room):
-        """Uses self.api to invite mxid to room."""
-        pass
+    def send_emote(self, *args, **kwargs):
+        """Sends m.room.message with msgtype m.emote."""
+        return self.api.send_emote(*args, **kwargs)
 
-    def redact(self, event_id):
-        """Uses self.api to redact a matrix event."""
-        pass
+    def send_notice(self, *args, **kwargs):
+        """Sends m.room.message with msgtype m.note."""
+        return self.api.send_notice(*args, **kwargs)
+
+    def set_room_name(self, *args, **kwargs):
+        """Sets room name.
+
+        Args:
+            room_id(str): The room ID
+            name(str): The new rom name
+            timestamp(int): Optional. Set origin_server_ts
+        """
+        return self.api.set_room_name(*args, **kwargs)
+
+    def set_room_topic(self, *args, **kwargs):
+        """Sets room topic.
+
+        Args:
+            room_id(str): The room ID
+            topic(str): The new room topic
+            timestamp(int): Optional. Set origin_server_ts (For application services only)
+        """
+        return self.api.set_room_topic(*args, **kwargs)
+
+    def leave_room(self, *args, **kwargs):
+        """Perform POST /rooms/$room_id/leave
+
+        Args:
+            room_id(str): The room ID
+        """
+        return self.api.leave_room(*args, **kwargs)
+
+    def invite_user(self, *args, **kwargs):
+        """Perform POST /rooms/$room_id/invite
+
+        Args:
+            room_id(str): The room ID
+            user_id(str): The user ID of the invitee
+        """
+        return self.api.invite_user(*args, **kwargs)
+
+    def kick_user(self, *args, **kwargs):
+        """Calls set_membership with membership="leave" for the user_id provided
+        """
+        return self.api.kick_user(*args, **kwargs)
+
+    def get_membership(self, room_id):
+        """Returns membership of user in room_id."""
+        return self.api.get_membership(self, room_id, self.mxid)
+
+    def set_membership(self, room_id, membership, **kwargs):
+        """Sets membership of self.mxid in room_id.
+
+        Args:
+            room_id(str): The room ID
+            membership(str): New membership value
+            reason(str): The reason
+            timestamp(int): Optional. Set origin_server_ts (For application services only)
+        """
+        return self.api.set_membership(room_id, self.mxid, membership, **kwargs)
+
+    def ban_user(self, *args, **kwargs):
+        """Perform POST /rooms/$room_id/ban
+
+        Args:
+            room_id(str): The room ID
+            user_id(str): The user ID of the banee(sic)
+            reason(str): The reason for this ban
+        """
+        return self.api.ban_user(*args, **kwargs)
+
+    def unban_user(self, *args, **kwargs):
+        """Perform POST /rooms/$room_id/unban
+
+        Args:
+            room_id(str): The room ID
+            user_id(str): The user ID of the banee(sic)
+        """
+        return self.api.unban_user(*args, **kwargs)
